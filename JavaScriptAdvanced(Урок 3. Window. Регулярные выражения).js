af0a3cea38e97a47c76b1cb654589f13c1812dff
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-10-20 17:14:36 +0300

    +3.4

--- JavaScriptAdvanced(Урок 3. Window. Регулярные выражения).js
+++ JavaScriptAdvanced(Урок 3. Window. Регулярные выражения).js
@@ -75,7 +75,36 @@ window.onload = function () {
 если на страницу заходят по адресу,
 page.html?a=100&b=200 то в теле страницы
 должно отображаться сообщение «сумма 300»
+
+function getQueryString() {
+    var args = {};                                  // пустой объект
+    // login=admin password=123
+    var query = location.search.substring(1);       // Получение строки запроса.
+    var pairs = query.split("&");                   // Разделение строки по амперсанду
+
+    for (var i = 0; i < pairs.length; i++) {
+
+        var pos = pairs[i].indexOf('=');            // Проверка пары "name=value"
+        if (pos == -1) {                            // Если не найдено - пропустить
+            continue;                    
+        }
+
+        var argname = pairs[i].substring(0, pos);   // Получение имени
+        var value = pairs[i].substring(pos + 1);    // Получение значения
+
+        args[argname] = value;                   // Сохранение как свойства
+
+    }
+    return args;
+}
+window.onload=function(){
+    let mass=getQueryString();
+    let sum=this.parseInt(mass.a)+this.parseInt(mass.b);
+    let p=document.getElementById("task32");    
+    p.innerHTML="Сумма "+sum;
+}
 */
+///////////////////////////////////////////////////////////////////////
 /*
 Задание 3
 Создайте приложение, которое от пользователя
@@ -83,7 +112,27 @@ page.html?a=100&b=200 то в теле страницы
 или «50 - 6». При получении такой строки
 приложение должно отобразить результат
 арифметического действия.
+
+window.onload = function () {
+    let t = document.getElementById("task33");
+    let str = t.value;
+    let pattern1 = new this.RegExp("\\+{1}");
+    let pattern2 = new this.RegExp("\\-{1}");
+    if (str.match(pattern1)) {
+        var pos = str.indexOf('+');            // Проверка пары "name=value"
+        var x1 = str.substring(0, pos);   // Получение имени
+        var x2 = str.substring(pos + 1);
+        t.value = this.parseInt(x1) + parseInt(x2);
+    }
+    if (str.match(pattern2)) {
+        var pos = str.indexOf('-');            // Проверка пары "name=value"
+        var x1 = str.substring(0, pos);   // Получение имени
+        var x2 = str.substring(pos + 1);
+        t.value = this.parseInt(x1) - parseInt(x2);
+    }
+}
 */
+//////////////////////////////////////////////////////////////////////
 /*
 Задание 4
 Создайте страницу с тремя ссылками:
@@ -96,7 +145,36 @@ page.html?a=100&b=200 то в теле страницы
 «JS Урок № (открыт)». При повторном
 нажатии на ссылку окно должно исчезать
 и текст ссылки должен меняться на исходный.
+
+window.onload = function () {
+    let get = function (a) {
+        return document.getElementById(a);
+    }
+    get("js1").onclick = function () {
+        if (document.location == get("js1").href) {
+            alert("JS Урок №1 (открыт)");
+        } else {
+            alert("Описание первого урока");
+        }
+
+    }
+    get("js2").onclick = function () {
+        if (document.location == get("js2").href) {
+            alert("JS Урок №2 (открыт)");
+        } else {            
+            alert("Описание второго урока");
+        }
+    }
+    get("js3").onclick = function () {
+        if (document.location == get("js3").href) {
+            alert("JS Урок №3 (открыт)");
+        } else {
+            alert("Описание третьего урока");
+        }
+    }
+}
 */
+///////////////////////////////////////////////////////////////////
 /*
 Задание 5
 Создайте бегущую строку. Пользователь
