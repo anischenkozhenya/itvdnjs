ff01e743729fad18b766f2e654d0ebb725db4c36
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-10-24 21:34:21 +0300

    +7

--- JavaScriptAdvanced(7. Формы и элементы формы).js
+++ JavaScriptAdvanced(7. Формы и элементы формы).js
@@ -9,24 +9,89 @@ email, пол (мужчина/женщина).
 При не удовлетворении этих правил форма должна выводить 
 сообщение об ошибке (не через alert). 
 */
+window.onload = function () {
+    let fname = this.document.task7.fname;
+    let txtinput = this.document.getElementsByTagName("input");
+    let pname = this.document.getElementsByTagName("span");
+    let lname = this.document.task7.lname;
+    let lgn = this.document.task7.lgn;
+    let password = this.document.task7.password;
+    let cpassword = this.document.task7.cpassword;
+    let email = this.document.task7.email;
+    let sex = this.document.task7.sex;
+    let watermark = "Введите данные";
+    let ck = false;
+    for (let i = 0; i < 6; i++) {
+        initInput();
+        function initInput() {
+            txtinput[i].value = watermark;
+            txtinput[i].style.color = "grey";
+            if (ck == true) {
+                // pname[i].style.opacity="1";
+            }
+        }
+        initInput();
+        txtinput[i].onfocus = function () {
+            if (txtinput[i].value == watermark) {
+                txtinput[i].value = "";
+                txtinput[i].style.color = "black";
+            }
+        }
+        txtinput[i].onblur = function () {
+            if (txtinput[i].value == "") {
+                 initInput();
+            }
+        }
+    }
+    ck = true;
+    this.document.getElementById("res").addEventListener("click", function () {
 
+    }, false);
+    let msg = "";
+    let ch = false;
+    this.document.getElementsByName("reg")[0].addEventListener("click", function () {
+        for (let i = 0; i < 6; i++) {
+            pname[i].style.opacity="0";
+            if (txtinput[i].value == "" || txtinput[i].value == watermark) {
+                msg += "Введены не все данные ";
+                ch=true;
+                pname[i].style.opacity="1";                
+            }
+        }
+        if (document.getElementsByName("sex")[0].value != "Men" && document.getElementsByName("sex")[0].value != "Women") {
+            msg += "Не выбран пол";
+            pname[6].style.opacity="1";
+            ch=true;
+        }
+        if (document.getElementsByName("password")[0].value!=document.getElementsByName("cpassword")[0].value) {
+            msg += "Пароли не совпадают";
+            ch=true;
+        }
+        if (ch) {
+            //alert(msg);
+            ch=false;
+        }
+        
+        msg = "";
+    }, false);
+}
 
 /*
-Задание 1 
-Разработайте страницу заказа пиццы.  
-Требования к странице:  
-Можно заказать три вида пиццы - маленькая, большая, средняя. 
-Можно самостоятельно выбрать ингредиенты к пицце - грибы, бекон, помидоры, сыр, оливки. 
-На странице должна вводиться информация о покупателе (его телефон и адрес доставки). 
- 
-Подумайте, какие данные следует проверять на странице, реализуйте эту проверку. 
-Также реализуйте подсчет стоимости пиццы, при выборе размеров и ингредиентов, автоматические должна пересчитываться сумма заказа. Цены на размер пиццы и отдельные ингредиенты выберите произвольно. 
+Задание 1
+Разработайте страницу заказа пиццы.
+Требования к странице:
+Можно заказать три вида пиццы - маленькая, большая, средняя.
+Можно самостоятельно выбрать ингредиенты к пицце - грибы, бекон, помидоры, сыр, оливки.
+На странице должна вводиться информация о покупателе (его телефон и адрес доставки).
+
+Подумайте, какие данные следует проверять на странице, реализуйте эту проверку.
+Также реализуйте подсчет стоимости пиццы, при выборе размеров и ингредиентов, автоматические должна пересчитываться сумма заказа. Цены на размер пиццы и отдельные ингредиенты выберите произвольно.
 /////////////////////////////////////////////////////////////
 */
 
 
 /*
-Задание 2 
-Реализуйте модуль для работы атрибута watermark, который будет отображать подсказку для поля ввода в который еще не вводилось значение. Поля вводов с применением модуля должны выглядеть так <input type=”text” name=”login” watermark=”Введите свой логин” /> 
-При загрузке страницы с такой формой в поле ввода должно отображаться сообщение «Введите свой логин», при клике на поле ввода это сообщение должно исчезат. 
+Задание 2
+Реализуйте модуль для работы атрибута watermark, который будет отображать подсказку для поля ввода в который еще не вводилось значение. Поля вводов с применением модуля должны выглядеть так <input type=”text” name=”login” watermark=”Введите свой логин” />
+При загрузке страницы с такой формой в поле ввода должно отображаться сообщение «Введите свой логин», при клике на поле ввода это сообщение должно исчезат.
  */
