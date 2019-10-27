76c9f9f3d56eb19e6fa2c66a53703a6067f4ee4d
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-10-27 23:14:20 +0300

    +71

--- JavaScriptAdvanced(7. Формы и элементы формы).js
+++ JavaScriptAdvanced(7. Формы и элементы формы).js
@@ -13,7 +13,7 @@ window.onload = function () {
     let fname = this.document.task7.fname;
     let txtinput = this.document.getElementsByTagName("input");
     let pname = this.document.getElementsByTagName("span");
-    let pattern=new this.RegExp("\b[a-z0-9._]+@[a-z0-9.-]+\.[a-z]{2,4}\b");    
+    let pattern = new this.RegExp("\b[a-z0-9._]+@[a-z0-9.-]+\.[a-z]{2,4}\b");
     let watermark = "Введите данные";
     let ck = false;
     for (let i = 0; i < 6; i++) {
@@ -41,7 +41,7 @@ window.onload = function () {
     ck = true;
     this.document.getElementById("res").addEventListener("click", function () {
     }, false);
-    let eml=this.document.getElementsByName("email")[0];    
+    let eml = this.document.getElementsByName("email")[0];
     let msg = "";
     let ch = false;
     this.document.getElementsByName("reg")[0].addEventListener("click", function () {
@@ -52,10 +52,10 @@ window.onload = function () {
                 ch = true;
                 pname[i].style.opacity = "1";
             }
-        } 
-        if(pattern.test(eml.value)==false){
+        }
+        if (pattern.test(eml.value) == false) {
             alert("sd");
-        }        
+        }
         if (document.getElementsByName("sex")[0].value != "Men" && document.getElementsByName("sex")[0].value != "Women") {
             msg += "Не выбран пол";
             pname[6].style.opacity = "1";
@@ -78,17 +78,56 @@ window.onload = function () {
 Разработайте страницу заказа пиццы.
 Требования к странице:
 Можно заказать три вида пиццы - маленькая, большая, средняя.
-Можно самостоятельно выбрать ингредиенты к пицце - грибы, бекон, помидоры, сыр, оливки.
-На странице должна вводиться информация о покупателе (его телефон и адрес доставки).
+Можно самостоятельно выбрать ингредиенты к пицце - грибы, бекон
+, помидоры, сыр, оливки.
+На странице должна вводиться информация о покупателе 
+(его телефон и адрес доставки).
 
-Подумайте, какие данные следует проверять на странице, реализуйте эту проверку.
-Также реализуйте подсчет стоимости пиццы, при выборе размеров и ингредиентов, автоматические должна пересчитываться сумма заказа. Цены на размер пиццы и отдельные ингредиенты выберите произвольно.
+Подумайте, какие данные следует проверять 
+на странице, реализуйте эту проверку.
+Также реализуйте подсчет стоимости пиццы, 
+при выборе размеров и ингредиентов, автоматические 
+должна пересчитываться сумма заказа. Цены на размер 
+пиццы и отдельные ингредиенты выберите произвольно.
 /////////////////////////////////////////////////////////////
 */
-
+window.onload = function () {
+    let prices = [10, 15, 20, 1, 1.5, 2.2, 1.5, 2.2];
+    let main = prices[0];
+    let totalprice = prices[0];
+    let input71 = this.document.getElementsByName("task71")[0].getElementsByTagName("input");
+    for (let i = 0; i < 3; i++) {
+        input71[i].onclick = function () {
+            if (input71[i].checked) {
+                totalprice -= main;
+                main = prices[i];
+                totalprice += main;
+            }
+            totalPriceHtml.innerHTML = totalprice + "рублей";
+        }
+    }
+    for (let i = 3; i < 8; i++) {
+        input71[i].onclick = function () {
+            if (input71[i].checked) {
+                totalprice += prices[i];
+            } else {
+                totalprice -= prices[i];
+            }
+            totalPriceHtml.innerHTML = totalprice + "рублей";
+        }
+    }
+    let totalPriceHtml = this.document.getElementsByName("totalprice")[0];
+    totalPriceHtml.innerHTML = totalprice + "рублей";
+}
 
 /*
 Задание 2
-Реализуйте модуль для работы атрибута watermark, который будет отображать подсказку для поля ввода в который еще не вводилось значение. Поля вводов с применением модуля должны выглядеть так <input type=”text” name=”login” watermark=”Введите свой логин” />
-При загрузке страницы с такой формой в поле ввода должно отображаться сообщение «Введите свой логин», при клике на поле ввода это сообщение должно исчезат.
+Реализуйте модуль для работы атрибута watermark,
+который будет отображать подсказку для поля ввода
+в который еще не вводилось значение. Поля вводов
+с применением модуля должны выглядеть так
+<input type=”text” name=”login” watermark=”Введите свой логин” />
+При загрузке страницы с такой формой в поле ввода
+должно отображаться сообщение «Введите свой логин»,
+при клике на поле ввода это сообщение должно исчезат.
  */
